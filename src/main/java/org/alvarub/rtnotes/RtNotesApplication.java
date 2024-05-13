6e31913a0d87725d40b418ae4aa893bf64e1d8fa
Alvaro Rubina <alvarorubina132@gmail.com>
2024-05-13 20:26:36 -0300

    Primer commit
    Configuraciones iniciales automáticas

---
+++ src/main/java/org/alvarub/rtnotes/RtNotesApplication.java
@@ -0,0 +1,13 @@
+package org.alvarub.rtnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alvarub/rtnotes/RtNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alvarub.rtnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
