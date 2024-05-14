5be7dc7a3e6cf585e2a67ba187a3294054b72318
Alvaro Rubina <alvarorubina132@gmail.com>
2024-05-13 21:23:22 -0300

    Mapeo de entidad Note
    Se mapeó la entidad Note, con sus principales atributos, al menos inicialmente, y su respectiva relación bidireccional con User

---
+++ src/main/java/org/alvarub/rtnotes/model/Note.java
@@ -0,0 +1,76 @@
+package org.alvarub.rtnotes.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Note {
+
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "note_gen")
+    @SequenceGenerator(name = "note_gen", sequenceName = "note_seq", allocationSize = 1)
+    private Long noteID;
+    private String title;
+    private String content;
+    private LocalDate date;
+
+    // Associations
+    @ManyToOne
+    @JoinColumn(name = "user_user_id")
+    private User user;
+
+    // Constructors
+    public Note() {
+
+    }
+
+    public Note(Long noteID, String title, String content, LocalDate date) {
+        this.noteID = noteID;
+        this.title = title;
+        this.content = content;
+        this.date = date;
+    }
+
+    // Get set
+    public Long getNoteID() {
+        return noteID;
+    }
+
+    public void setNoteID(Long noteID) {
+        this.noteID = noteID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
