7a87c96a81d6ea4234d76a8b049fed3b4cdaf46e
Alvaro Rubina <alvarorubina132@gmail.com>
2024-07-04 21:38:14 -0300

    Creada clase UserDTO
    Se creó la clase UserDTO junto con sus atributos iniciales

---
+++ src/main/java/org/alvarub/rtnotes/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package org.alvarub.rtnotes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class UserDTO {
+
+    // Attributes
+    private String userName;
+    private String name;
+    private String lastName;
+}
