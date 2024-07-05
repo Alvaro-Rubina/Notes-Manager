1d6eec4c6b15f8958c16de137c5b827a9f7d7ca2
Alvaro Rubina <alvarorubina132@gmail.com>
2024-07-05 01:34:50 -0300

    Actualizados metodos del INoteService
    Se cambiaron las clases que devuelven los metodos findNote y getNotes de de la interfaz INoteService: Ahora devuelven DTOs

--- src/main/java/org/alvarub/rtnotes/service/INoteService.java
+++ src/main/java/org/alvarub/rtnotes/service/INoteService.java
@@ -1,13 +1,14 @@
 package org.alvarub.rtnotes.service;
 
+import org.alvarub.rtnotes.dto.NoteDTO;
 import org.alvarub.rtnotes.model.Note;
 import java.util.List;
 
 public interface INoteService {
 
     public void saveNote(Note note);
-    public Note findNote(int id);
-    public List<Note> getNotes();
+    public NoteDTO findNote(int id);
+    public List<NoteDTO> getNotes();
     public void deleteNote(int id);
     public void editNote(Note note);
 }
