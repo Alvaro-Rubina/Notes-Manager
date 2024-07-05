1a1a0e49995da8d62b084a050025d234d3fbb5a5
Alvaro Rubina <alvarorubina132@gmail.com>
2024-07-04 21:37:33 -0300

    Modificacion de atributos en User
    Se realizaron cambios en los atributos de la clase User, principalmente se comentaron los atributos de email y password ya que aún no son necesarios.
    Se agregaron los atributos name y lastName

--- src/main/java/org/alvarub/rtnotes/model/User.java
+++ src/main/java/org/alvarub/rtnotes/model/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,8 +21,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_gen")
     @SequenceGenerator(name = "user_gen", sequenceName = "user_seq", allocationSize = 1)
     private Long userID;
-    private String username;
-    private String password;
+    private String userName;
+    private String name;
+    private String lastName;
+    private LocalDate accountCreationDate;
+
+    // TODO: Estos atributos se agregarán cuando se implemente la autenticación.
+    /*private String email;
+    private String password;*/
+
 
     // Associations
     @OneToMany(mappedBy = "user", orphanRemoval = true)
