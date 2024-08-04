ec2fd04cbbc2405f5cc7a2dbe7f4dc16ceb134be
Alvaro Rubina <alvarorubina132@gmail.com>
2024-08-04 00:59:14 -0300

    Añadida descripción a cada endpoint
    Se añadió mediante la etiqueta @Operation una breve descripción a cada endpoint para que se refleje mediante swagger

--- src/main/java/org/alvarub/notesmanager/controller/NoteController.java
+++ src/main/java/org/alvarub/notesmanager/controller/NoteController.java
@@ -1,5 +1,8 @@
 package org.alvarub.notesmanager.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.alvarub.notesmanager.dto.NoteDTO;
 import org.alvarub.notesmanager.exception.UserNotFoundException;
 import org.alvarub.notesmanager.model.Note;
@@ -17,6 +20,7 @@ public class NoteController {
     @Autowired
     private INoteService noteService;
 
+    @Operation(summary = "Guardar una nueva nota")
     @PostMapping("/notes/new")
     public ResponseEntity<String> saveNote(@RequestBody Note note) {
         try {
@@ -27,24 +31,28 @@ public class NoteController {
         }
     }
 
+    @Operation(summary = "Buscar una nota a través de su ID")
     @GetMapping("/notes/find/{id}")
     @ResponseBody
     public NoteDTO findNote(@PathVariable int id) {
         return noteService.findNote(id);
     }
 
+    @Operation(summary = "Obtener todas las notas")
     @GetMapping("/notes/find-all")
     @ResponseBody
     public List<NoteDTO> findAllNotes() {
         return noteService.getNotes();
     }
 
+    @Operation(summary = "Eliminar una nota")
     @DeleteMapping("/notes/delete/{id}")
     public String deleteNote(@PathVariable int id) {
         noteService.deleteNote(id);
         return "Nota eliminada con éxito";
     }
 
+    @Operation(summary = "Actualizar una nota")
     @PutMapping("/notes/edit")
     public String updateNote(@RequestBody Note note) {
         noteService.editNote(note);
--- src/main/java/org/alvarub/notesmanager/controller/UserController.java
+++ src/main/java/org/alvarub/notesmanager/controller/UserController.java
@@ -1,5 +1,6 @@
 package org.alvarub.notesmanager.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.alvarub.notesmanager.dto.UserDTO;
 import org.alvarub.notesmanager.model.User;
 import org.alvarub.notesmanager.service.IUserService;
@@ -14,30 +15,35 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
+    @Operation(summary = "Guardar un nuevo usuario")
     @PostMapping("/users/new")
     public String saveUser(@RequestBody User user) {
         userService.saveUser(user);
         return "Usuario registrado!";
     }
 
+    @Operation(summary = "Buscar un usuario a través de su ID")
     @GetMapping("/users/find/{id}")
     @ResponseBody
     public UserDTO findUser(@PathVariable int id) {
         return userService.findUser(id);
     }
 
+    @Operation(summary = "Obtener todos los usuarios")
     @GetMapping("/users/find-all")
     @ResponseBody
     public List<UserDTO> getAllUsers() {
         return userService.getUsers();
     }
 
+    @Operation(summary = "Eliminar un usuario")
     @DeleteMapping("/users/delete/{id}")
     public String deleteUser(@PathVariable int id) {
         userService.deleteUser(id);
         return "Usuario eliminado con éxito.";
     }
 
+    @Operation(summary = "Actualizar un usuario")
     @PutMapping("/users/edit")
     public String editUser(@RequestBody User user) {
         userService.editUser(user);
