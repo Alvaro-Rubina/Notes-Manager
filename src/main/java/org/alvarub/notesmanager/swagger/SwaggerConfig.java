bb0b3555a178c9329b2ebc7ead7bbdd71f46561a
Alvaro Rubina <alvarorubina132@gmail.com>
2024-08-03 23:15:24 -0300

    Creado SwaggerConfig
    Se creó una clase SwaggerConfig donde se definen algunos datos informativos sobre la construcción de la api

---
+++ src/main/java/org/alvarub/notesmanager/swagger/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package org.alvarub.notesmanager.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Notes Manger")
+                        .description("API desarrollada con Spring Boot que permite simular un gestor de notas, " +
+                                "permitiendo llevar un control de los usuarios y sus respectivas notas" +
+                                " mediante consultas a una base de datos MySQL.")
+                        .contact(new Contact()
+                                .name("Alvaro Rubina")
+                                .email("alvarorubina132@gmail.com")
+                                .url("https://github.com/Alvaro-Rubina")));
+    }
+}
