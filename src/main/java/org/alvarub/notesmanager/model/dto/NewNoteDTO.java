340b7c6f5c9b353577a456b4842a3b5bc265b398
Alvaro Rubina <alvarorubina132@gmail.com>
2024-08-13 06:53:14 -0300

    Añadidas restricciones a los DTO
    Se añadieron las validaciones de datos obligatorios a los DTO de entrada.

--- src/main/java/org/alvarub/notesmanager/model/dto/NewNoteDTO.java
+++ src/main/java/org/alvarub/notesmanager/model/dto/NewNoteDTO.java
@@ -1,9 +1,12 @@
 package org.alvarub.notesmanager.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record NewNoteDTO(@Schema(example = "A silly note") String title,
-                         @Schema(example = "Blablabla") String content,
-                         @Schema(example = "1") Long creatorID) {
+public record NewNoteDTO(@Schema(example = "A silly note") @NotBlank(message = "Title is mandatory") String title,
+                         @Schema(example = "Blablabla") @NotBlank(message = "Content is mandatory") @Size(max = 500, message = "Content must be at most 500 characters") String content,
+                         @Schema(example = "1") @NotNull(message = "CreatorID is mandatory") Long creatorID) {
 
 }
