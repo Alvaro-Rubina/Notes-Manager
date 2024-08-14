a37baa07ffe5a07be9aae34aa22868183bccb045
Alvaro Rubina <alvarorubina132@gmail.com>
2024-08-13 21:27:13 -0300

    Traducida descripción de la API en SwaggerConfig
    Se tradujo al inglés la descripción de la API dentro de la clase SwaggerConfig.

--- src/main/java/org/alvarub/notesmanager/utils/exception/GlobalExceptionHandler.java
+++ src/main/java/org/alvarub/notesmanager/utils/exception/GlobalExceptionHandler.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
